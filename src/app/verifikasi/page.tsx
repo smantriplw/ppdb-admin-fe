8d5968919c10224cf5914b497241e70c1d46e8af
hanifdwyputras <hanifdwyputrasembiring@gmail.com>
2023-06-09 04:47:59 +0000

    feat: add ket
    Signed-off-by: hanifdwyputras <hanifdwyputrasembiring@gmail.com>

--- src/app/verifikasi/page.tsx
+++ src/app/verifikasi/page.tsx
@@ -18,8 +18,15 @@ export default function VerifikasiPage() {
     const [currentPage, setCurrentPage] = React.useState(1);
     const [query, setQuery] = React.useState<string>();
     const [selectedData, setSelectedData] = React.useState<any[]>([]);
+    const [ket, setKet] = React.useState<{
+        message: string;
+        created_at: string;
+        isSafe: boolean;
+    }>();
     const [sortBy, setSortBy] = React.useState<string>('verificator_id:== \'null\'');
     const [current, setCurrent] = React.useState<string>();
+    const [userstate, setUserState] = React.useState<any>();
+
     const { data, isLoading, mutate } = useSWR(session.token ? `/api/archives?offset=200&page=${currentPage}&all=true` : null, url => fetcher(url, {
         headers: {
             'Content-Type': 'application/json',
@@ -66,6 +73,18 @@ export default function VerifikasiPage() {
         if (modalDom) {
             modalDom.setAttribute('open', '');
             setCurrent(id);
+
+            if (ket && !selectedData.find((x: any) => x.id === current)?.verificator)
+                setKet(undefined);
+
+            if (ev.currentTarget.innerText.trim().replace(/\s+/g, '').toLowerCase() === 'ket')
+            {
+                const verifyData = data?.archives.find((x: any) => x.id === id)?.verify_data;
+
+                if (verifyData) {
+                    setKet(verifyData);
+                }
+            }
         }
     }
 
@@ -106,6 +125,10 @@ export default function VerifikasiPage() {
         cariForm(query || '');
     }, [query, cariForm]);
 
+    React.useEffect(() => {
+        setUserState(data?.archives.find((x: any) => x.id === current));
+    }, [current]);
+
     return (
         <div>
             <div className="lg:px-40">
@@ -133,7 +156,7 @@ export default function VerifikasiPage() {
 
                                     <div className="join-item">
                                         <select className="select w-full max-w-xs" value={sortBy} onChange={(ev) => setSortBy(ev.target.value)}>
-                                            <option disabled>Sort by:</option>
+                                            <option disabled>Sort by:</option>991f716e-66d4-4b88-8fb0-884f83c4ae5b
                                             <option value="type:== 'zonasi'">Zonasi</option>
                                             <option value="type:== 'prestasi'">Prestasi</option>
                                             <option value="type:== 'afirmasi'">Afirmasi</option>
@@ -159,8 +182,7 @@ export default function VerifikasiPage() {
                                     <th>Nama</th>
                                     <th>NISN</th>
                                     <th>NIK</th>
-                                    <th>Nama Ayah</th>
-                                    <th>Nama Ibu</th>
+                                    <th>Telp</th>
                                     <th>Asal Sekolah</th>
                                     <th>TTL</th>
                                     <th>Jalur</th>
@@ -177,16 +199,15 @@ export default function VerifikasiPage() {
                                                 {index+1}
                                             </th>
                                             <th>
-                                                <button disabled={Boolean(archive.verificator_id)} className="btn btn-secondary text-sm" onClick={(ev) => handleVerify(ev, archive.id)}>
-                                                    verify
+                                                <button disabled={sortBy !== 'verificator_id:!== \'null\'' && Boolean(archive.verificator_id)} className={`btn btn-${archive.verificator_id ? 'accent' : 'secondary'} text-sm`} onClick={(ev) => handleVerify(ev, archive.id)}>
+                                                    {Boolean(archive.verificator_id) ? 'ket' : 'verify'}
                                                 </button>
                                             </th>
                                             <td>{archive.id.split('-')[0]}/{archive.gender}</td>
                                             <td>{archive.name}</td>
                                             <td>{archive.nisn}</td>
                                             <td>{archive.nik}</td>
-                                            <td>{archive.father_name}</td>
-                                            <td>{archive.mother_name}</td>
+                                            <td>0{archive.phone}</td>
                                             <td>{archive.school}</td>
                                             <td>{archive.birthday}</td>
                                             <td>{archive.type.toUpperCase()}</td>
@@ -219,18 +240,37 @@ export default function VerifikasiPage() {
                 <form method="dialog" className="modal-box">
                     {/* <button htmlFor="verify_modal" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button> */}
                     <h3 className="font-bold text-lg">
-                        Apakah Anda yakin bahwa <span className="font-semibold">&apos;{selectedData.find((x: any) => x.id === current)?.name || current}&apos;</span> telah terverifikasi dengan benar?
+                        {!ket || !selectedData.find((x: any) => x.id === current)?.verificator ? (
+                            <>
+                                Apakah Anda yakin bahwa <span className="font-semibold">&apos;{selectedData.find((x: any) => x.id === current)?.name || current}&apos;</span> telah terverifikasi dengan benar?
+                            </>) : (
+                                <span>
+                                    Peserta: {selectedData.find((x: any) => x.id === current)?.name || current}
+                                </span>
+                            )}
                     </h3>
                     <button onClick={() => document.getElementById('verify_modal')?.removeAttribute('open')} type="button" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                     <div className="py-4">
                         <p>
-                            Sebelum Anda memverifikasi peserta, pastikan Anda telah melihat dengan seksama dokumen yang telah tersedia tentang peserta didik ini. Seperti dokumen SKHU, Foto Pas, dan KK/Sertifikat/SK Mutasi/KIP.
+                            {!ket || !selectedData.find((x: any) => x.id === current)?.verificator ? <span>Sebelum Anda memverifikasi peserta, pastikan Anda telah melihat dengan seksama dokumen yang telah tersedia tentang peserta didik ini. Seperti dokumen SKHU, Foto Pas, dan KK/Sertifikat/SK Mutasi/KIP.</span> : (
+                                <span>
+                                    Diverifikasi oleh <span className="uppercase font-semibold">{selectedData.find((x: any) => x.id === current)?.verificator?.username || '-'}</span>, pada <span className="font-semibold">{new Date(ket.created_at).toLocaleTimeString('id-ID', {
+                                        day: '2-digit',
+                                        month: 'long',
+                                        hour: '2-digit',
+                                        minute: '2-digit',
+                                        second: '2-digit',
+                                    }).replace(/\./g, ':')}</span>
+                                </span>
+                            )}
                         </p>
                     </div>
 
                     <Formik
                         validationSchema={verifySchema}
                         onSubmit={(values, actions) => {
+                            if (!userstate) return;
+
                             actions.setErrors({});
                             const message = values.isSafe ? undefined : values.message;
 
@@ -255,18 +295,18 @@ export default function VerifikasiPage() {
                             <Form onSubmit={props.handleSubmit}>
                                 <div className="form-control">
                                     <label htmlFor="isSafe">Apakah aman?</label>
-                                    <Field disabled={props.isSubmitting} as="input" type="checkbox" className="checkbox" name="isSafe" />
+                                    <Field checked={ket?.isSafe} disabled={props.isSubmitting || Boolean(ket)} as="input" type="checkbox" className="checkbox" name="isSafe" />
                                 </div>
 
                                 <div className={`form-control mt-2${props.values.isSafe ? ' hidden': ''}`}>
                                     <label htmlFor="message">Keterangan</label>
-                                    <Field disabled={props.isSubmitting} as="textarea" name="message" className="textarea textarea-bordered" placeholder="Pesan untuk berkas ini. Contoh: KK tidak masuk zona" />
+                                    <Field value={ket?.message} disabled={props.isSubmitting || Boolean(ket)} as="textarea" name="message" className="textarea textarea-bordered" placeholder="Pesan untuk berkas ini. Contoh: KK tidak masuk zona" />
                                     <ErrorMessage name="message" className="text-red-500" />
                                 </div>
 
-                                <div className="modal-action">
+                                {!Boolean(ket) && <div className="modal-action">
                                     <button type="button" onClick={props.submitForm} className="btn btn-primary" disabled={props.isSubmitting}>{props.isSubmitting ? <span className="loading loading-dots loading-sm"></span> : 'verify'}</button>
-                                </div>
+                                </div>}
                             </Form>
                         ))}
                     </Formik>
